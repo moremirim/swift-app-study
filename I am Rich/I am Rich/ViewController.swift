949a8fb92bb082f0f368b8e362bf11f1914f64a6
moremirim <moremirim@gmail.com>
2024-04-17 19:12:44 +0900

    Upload I am Rich only for dp app

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 박미림 on 4/8/24.
+//
+
+import UIKit
+//
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
